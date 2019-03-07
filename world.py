6cdf63681a8c9f5e8aeccba8e2caca7339df1254
Clayton Townsend II <25651218+ctII@users.noreply.github.com>
2019-03-07 16:04:16 -0600

    Add horrible terrain system

--- world.py
+++ world.py
@@ -1,5 +1,7 @@
 from terrainBarrier import terrainBarrier
 
+#entire terrain system needs to be redone, getTerrainAt is currently just bruteforcing
+
 class world:
     def __init__(self, width, height):
         self.entities = []
@@ -15,6 +17,11 @@ class world:
 
     def getTerrain(self):
         return self.terrain
+
+    def getTerrainAt(self, posY, posX):
+        for terrain in self.terrain:
+            if terrain.getX() == posX and terrain.getY() == posY:
+                return terrain
 
     def getEntity(self, UUID):
         for entity in self.entities:
